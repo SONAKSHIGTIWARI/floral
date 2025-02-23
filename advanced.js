85be16ee705256b1dc5220f208562ac57c23fe47
SONAKSHIGTIWARI <142566567+SONAKSHIGTIWARI@users.noreply.github.com>
2025-02-23 22:56:17 +0530

    Add files via upload

---
+++ advanced.js
@@ -0,0 +1,46 @@
+/*console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+
+let age=12;
+let name="sonakshi";
+let exp=5;
+console.log(age);
+console.log(name);
+console.log(exp);
+console.log("Start");
+function sync(){
+    console.log("code is running...");
+}
+sync();
+console.log("end");
+
+console.log("Start");
+setTimeout(function(){
+    console.log("code is running...");
+},2000)
+console.log("End");
+
+console.log("Start");
+
+setTimeout(function()
+{
+    try{
+        nole.log("Code is running..");
+    }
+    catch(error)
+    {
+        console.log(error.message);
+    }
+    finally{
+        console.log("Code");
+    }
+},2000);
+
+console.log("end");
+*/
+setTimeout(()=>{
+    console.log("hell");
+},0)
